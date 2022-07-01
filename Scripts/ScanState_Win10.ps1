c26dd2b479eb4b7e2d14b3fd9fee9f8441f21447
Christian Kunis <engineering@lixiehammers.com>
2022-07-01 08:53:35 -0400

    Remove commented out old syntax

--- Scripts/ScanState_Win10.ps1
+++ Scripts/ScanState_Win10.ps1
@@ -1,6 +1,5 @@
 $LocalUserName = $args[0]
 
-# . (Join-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) 'CloneUSMT.ps1')
 . (Join-Path "$PSScriptRoot" 'CloneUSMT.ps1')
 
 $ProgFilePath = (Join-Path "$MigPath" ("$env:COMPUTERNAME" + "_" + (Split-Path -Path "$LocalUserName" -Leaf) + "_prog.log"))
