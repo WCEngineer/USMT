1c386476c1b9fdae5c23b04fd14007129334b767
Christian Kunis <ckunis98@gmail.com>
2022-07-10 16:49:50 -0400

    Updated log file names to be more concise

--- Scripts/ScanState_Win10.ps1
+++ Scripts/ScanState_Win10.ps1
@@ -2,8 +2,8 @@ $LocalUserName = $args[0]
 
 . (Join-Path "$PSScriptRoot" 'CloneUSMT.ps1')
 
-$ProgFilePath = (Join-Path "$MigPath" ("$env:COMPUTERNAME" + "_" + (Split-Path -Path "$LocalUserName" -Leaf) + "_prog.log"))
-$LogFilePath = (Join-Path "$MigPath" ("$env:COMPUTERNAME" + "_" + (Split-Path -Path "$LocalUserName" -Leaf) + "_scan.log"))
+$ProgFilePath = (Join-Path "$MigPath" ("prog_" + (Split-Path -Path "$LocalUserName" -Leaf) + ".log"))
+$LogFilePath = (Join-Path "$MigPath" ("scan_" + (Split-Path -Path "$LocalUserName" -Leaf) + ".log"))
 
 Set-Location "$LocalExecutablePath"
 .\scanstate.exe "$MigPath" /i:MigUser.xml /i:MigApp.xml /i:MigAppData.xml /v:13 /progress:"$ProgFilePath" /l:"$LogFilePath" /o /localonly /c /uel:100 #/ue:*\* /ui:"$LocalUserName"
